0cbfc96acf7a1959602f4b8a37660b8794880a4d
Laylooo <khodjaeva.laylo@gmail.com>
2021-08-19 01:09:14 +0500

    add webpack to the project

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
+
+  },
+  // devtool: 'inline-source-map',
+  // devServer: {
+	//      contentBase: './dist',
+	//  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  resolve: {
+	extensions: [ '.js', '.jsx']
+ },
+};
