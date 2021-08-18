53c2da74d9f8142e2443d1684caa14998a6ca85b
Laylooo <khodjaeva.laylo@gmail.com>
2021-08-19 04:25:54 +0500

    Add Javascript and styles

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 import _ from 'lodash';
 import './style.css';
 
+const tasksList = [
+  {
+    description: 'Do homework',
+    completed: false,
+    index: null,
+  },
+  {
+    description: 'Attend Microverse classes',
+    completed: false,
+    index: null,
+  },
+  {
+    description: 'Wash dishes',
+    completed: false,
+    index: null,
+  },
+  {
+    description: 'Do my bed',
+    completed: false,
+    index: null,
+  },
+];
+
+
+const checkTasks = () => {
+  const ul = document.querySelector('ul')
+  for(let i = 0; i < tasksList.length; i+=1) {
+    tasksList[i].index = i;
+    ul.innerHTML += `<li><input type="checkbox"><h3>${tasksList[i].description}</h3><img src="https://img.icons8.com/ios-glyphs/30/000000/menu-2.png"/></li>`;
+  }
+};
+
+checkTasks()
